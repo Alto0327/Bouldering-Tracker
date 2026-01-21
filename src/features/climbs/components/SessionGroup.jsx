0f344bc4b2282b6c4a966d701bf3ae91c1bba74a
Alto0327 <aj.silvaf2003@gmail.com>
2026-01-21 12:59:39 -0500

    Refactor: Date formatting UX

--- src/features/climbs/components/SessionGroup.jsx
+++ src/features/climbs/components/SessionGroup.jsx
@@ -1,10 +1,18 @@
 import ClimbItem from "./ClimbItem"
 
 export default function SessionGroup({sessions}){
-    // console.log(sessions)
+    function formatDate(isoDate) {
+        return new Date(isoDate).toLocaleDateString("en-US", {
+            weekday: "long",
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+        })
+    }
+    
     const sessionElement = sessions.map(session =>(
         <div key={session.date}>
-            <h1>{session.date}</h1>
+            <h1>{formatDate(session.date)} Session</h1>
             {session.climbs.map(climb =>
                 <ClimbItem key={climb.id} climb={climb} />
             )}
